2d69f4c6a8eb1b8318aee3b911e83b664edb010e
BjarkiHrafn <bjarki15@ru.is>
2018-12-11 12:17:31 +0000

    css runk

--- ControllerClass.py
+++ ControllerClass.py
@@ -2,10 +2,10 @@ class Controller(object):
     answer = ""
 
     def DeployQuestion(self):
-        someList = ["skr skrr",
-            "ey mang",
-            "Shit mang , that shit is wack mang",
-            "brrRRaah"]
+        someList = [{"Question" :["skr skrr",
+                    "ey mang",
+                    "Shit mang , that shit is wack mang uashd soajdo sao odoo dso od oasas ddo aos doa ods odsd do do",
+                    "brrRRaah"]}]
 
         return someList
 
